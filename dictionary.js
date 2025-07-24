c392499751b0e55c1cdbdbee1fd583a5850ffa2d
arjun939201 <throwuse829@gmail.com>
2025-07-24 11:35:34 +0530

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,7 @@
+const dictionary = [
+  { telugu: "నుడి", transliteration: "nuḍi", type: "n.", meaning: "language" },
+  { telugu: "చదువు", transliteration: "chaduvu", type: "n.", meaning: "education" },
+  { telugu: "తిండి", transliteration: "tiṇḍi", type: "n.", meaning: "food" },
+  { telugu: "స్నేహం", transliteration: "snēhaṁ", type: "n.", meaning: "friendship" },
+  { telugu: "నెసరు", transliteration: "nesaru", type: "n.", meaning: "sun" }
+];
